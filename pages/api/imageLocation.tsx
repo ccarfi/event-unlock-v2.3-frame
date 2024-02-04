e97862ccc5c0aebfc87df598085cef200d35f990
Christopher Carfi <ccarfi@gmail.com>
2024-02-03 19:46:13 -0800

    Update imageLocation.tsx

--- pages/api/imageLocation.tsx
+++ pages/api/imageLocation.tsx
@@ -1,1 +1,17 @@
+import { generateOGImage } from '@vercel/og';
 
+export default async function handler(req, res) {
+  //  const { title, author } = req.query;
+    
+    const html = `
+        <div style="display: flex; align-items: center; justify-content: center; font-family: sans-serif;">
+            <h1>This is the location H1</h1>
+            <p>This is the location body</p>
+        </div>
+    `;
+
+    const imageBuffer = await generateOGImage({ html });
+
+    res.setHeader('Content-Type', 'image/png');
+    res.send(imageBuffer);
+}
