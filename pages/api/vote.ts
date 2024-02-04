486e38506c841bc659cc4185eb4a737a71133923
Christopher Carfi <ccarfi@gmail.com>
2024-02-04 13:55:43 -0800

    Update vote.ts

--- pages/api/vote.ts
+++ pages/api/vote.ts
@@ -127,7 +127,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const isMember = balances.some((balance) => balance > 0);
             
             let action = "";
-            let button1Action = "";
+            let button1Action = "post";
             if (buttonId === 1) {
                 action = "register";
                 if (isMember) {
