b02bc8a0fb29d2eabbdf42fd592a28a20654fc04
Christopher Carfi <ccarfi@gmail.com>
2024-02-03 21:00:31 -0800

    Update imageLocation.tsx

--- pages/api/imageLocation.tsx
+++ pages/api/imageLocation.tsx
@@ -7,21 +7,24 @@ export const config = {
 export default async function handler() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        You pressed the Location button
-      </div>
+//      <div
+//        style={{
+//          fontSize: 40,
+//          color: 'black',
+//          background: 'white',
+//          width: '100%',
+//          height: '100%',
+//          padding: '50px 200px',
+//          textAlign: 'center',
+//          justifyContent: 'center',
+//          alignItems: 'center',
+//        }}
+//      >
+//        You pressed the Location button
+//      </div>
+      <div>
+        <img src="https://i.imgur.com/fKUBgay.png">
+      </div>  
     ),
     {
       width: 1200,
