11eac183fbe00a635cfcb70b91a97f3e33ce926c
Christopher Carfi <ccarfi@gmail.com>
2024-02-05 11:34:56 -0800

    Update page.tsx

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -106,10 +106,11 @@ export default async function Page({params}: { params: {id: string}}) {
         <>
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
-                    <PollVoteForm poll={poll}/>
                 </main>
             </div>
         </>
     );
 
 }
+
+/* moved from line 109             <PollVoteForm poll={poll}/>      */
