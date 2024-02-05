1e6a5afc503d9c42d97afca95d4fcd8bc5d4c3aa
Christopher Carfi <ccarfi@gmail.com>
2024-02-05 09:30:56 -0800

    Update form.tsx

--- app/form.tsx
+++ app/form.tsx
@@ -318,7 +318,6 @@ export function PollVoteForm({poll, viewResults}: { poll: Poll, viewResults?: bo
                     });
                 }}
             >
-              //  {state.showResults ? <PollResults poll={poll}/> : <PollOptions poll={poll} onChange={handleVote}/>}
                 {state.showResults ? <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                         type="submit"
@@ -335,4 +334,6 @@ export function PollVoteForm({poll, viewResults}: { poll: Poll, viewResults?: bo
         </div>
 );
 }
+
+/*  removed this at line 321               {state.showResults ? <PollResults poll={poll}/> : <PollOptions poll={poll} onChange={handleVote}/>} */
 
