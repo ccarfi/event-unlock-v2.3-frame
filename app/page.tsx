1cbfddf596e6d792d9532683b1a7d7002ec35050
Christopher Carfi <ccarfi@gmail.com>
2024-02-05 19:18:55 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -114,29 +114,6 @@ z"/>
   );
 }
 
-/*
-function VercelLogo(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      aria-label="Vercel Logo"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 19"
-      {...props}
-    >
-      <path
-        clipRule="evenodd"
-        d="M12.04 2L2.082 18H22L12.04 2z"
-        fill="#000"
-        fillRule="evenodd"
-        stroke="#000"
-        strokeWidth="1.5"
-      />
-    </svg>
-  );
-}
-*/
-
 export default async function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
