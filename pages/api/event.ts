66e0ac8e7b6aee242f75b2680dd70d0915582ad6
Christopher Carfi <ccarfi@gmail.com>
2024-02-06 04:35:17 -0800

    Update event.ts

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -14,7 +14,7 @@ const client = HUB_URL ? getSSLHubRpcClient(HUB_URL) : undefined;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        // Process the vote
+
         // For example, let's assume you receive an option in the body
         try {
             const pollId = req.query['id']
