9e8d706553839b721beb57e2257d023a1e385d97
Christopher Carfi <ccarfi@gmail.com>
2024-02-05 18:50:45 -0800

    Update form.tsx

--- app/form.tsx
+++ app/form.tsx
@@ -23,112 +23,6 @@ type EventState = {
   voted?: boolean;
 };
 
-/*
-export function EventCreateForm() {
-  let formRef = useRef<HTMLFormElement>(null);
-  let [state, mutate] = useOptimistic(
-      { pending: false },
-      function createReducer(state, newEvent: EventState) {
-        if (newEvent.newEvent) {
-          return {
-            pending: newEvent.pending,
-          };
-        } else {
-          return {
-            pending: newEvent.pending,
-          };
-        }
-      },
-  );
-
-  let eventStub = {
-    id: uuidv4(),
-    created_at: new Date().getTime(),
-    title: "",
-    contractAddress: "",
-    network: 0,
-    checkoutURL: "",
-    eventImageURL: "",
-    registeredImageURL: "",
-    registeredLocationImageURL: "",
-    registeredTicketImageURL: "",
-  };
-  let saveWithNewEvent = saveEvent.bind(null, eventStub);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let [isPending, startTransition] = useTransition();
-
-  return (
-      <>
-        <div className="mx-8 w-full">
-          <form
-              className="relative my-8"
-              ref={formRef}
-              action={saveWithNewPoll}
-              onSubmit={(event) => {
-                event.preventDefault();
-                let formData = new FormData(event.currentTarget);
-                let newPoll = {
-                  ...eventStub,
-                  title: formData.get("title") as string,
-                  option1: formData.get("option1") as string,
-                  option2: formData.get("option2") as string,
-                  option3: formData.get("option3") as string,
-                  option4: formData.get("option4") as string,
-                  votes1: 0,
-                  votes2: 0,
-                  votes3: 0,
-                  votes4: 0,
-                  contractAddress: formData.get("contractAddress") as string,,
-                  network:0,
-                  checkoutURL: formData.get("checkoutURL") as string,,
-                  eventImageURL: formData.get("eventImageURL") as string,,
-                  registeredImageURL: formData.get("registeredImageURL") as string,,
-                  registeredLocationImageURL: formData.get("registeredLocationImageURL") as string,,
-                  registeredTicketImageURL: formData.get("registeredTicketImageURL") as string,,
-                };
-
-                formRef.current?.reset();
-                startTransition(async () => {
-                  mutate({
-                    newPoll,
-                    pending: true,
-                  });
-
-                  await savePoll(newPoll, formData);
-                });
-              }}
-          >
-            <input
-                aria-label="Event Title"
-                className="pl-3 pr-28 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
-                maxLength={150}
-                placeholder="Title..."
-                required
-                type="text"
-                name="title"
-            />
-              <div className={"pt-2 flex justify-end"}>
-                  <button
-                      className={clsx(
-                          "flex items-center p-1 justify-center px-4 h-10 text-lg border bg-blue-500 text-white rounded-md w-24 focus:outline-none focus:ring focus:ring-blue-300 hover:bg-blue-700 focus:bg-blue-700",
-                          state.pending && "bg-gray-700 cursor-not-allowed",
-                      )}
-                      type="submit"
-                      disabled={state.pending}
-                  >
-                      Create
-                  </button>
-              </div>
-          </form>
-        </div>
-          <div className="w-full">
-          </div>
-      </>
-  );
-}
-*/
-
-
 
 export function PollCreateForm() {
   let formRef = useRef<HTMLFormElement>(null);
@@ -148,7 +42,6 @@ export function PollCreateForm() {
   );
 
   /* ========================================= */
-  /* THIS IS TO TEST DATABASE WRITE FOR EVENTS */
 
     let eventStub = {
     id: uuidv4(),
@@ -180,6 +73,10 @@ export function PollCreateForm() {
     votes4: 0,
   };
   let saveWithNewPoll = savePoll.bind(null, pollStub);
+
+  /* ========================================= */
+
+  
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let [isPending, startTransition] = useTransition();
 
@@ -223,13 +120,22 @@ export function PollCreateForm() {
               }}
           >
             <input
-                aria-label="Poll Title"
+                aria-label="Event Title"
                 className="pl-3 pr-28 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
                 maxLength={150}
                 placeholder="Title..."
                 required
                 type="text"
                 name="title"
+            />
+            <input
+                aria-label="Contract Address"
+                className="pl-3 pr-28 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
+                maxLength={150}
+                placeholder="0x..."
+                required
+                type="text"
+                name="contractAddress"
             />
               <div className={"pt-2 flex justify-end"}>
                   <button
