537c185e05195b7840a2424a5c41c7f2123ef0de
Christopher Carfi <ccarfi@gmail.com>
2024-02-05 19:12:10 -0800

    Update form.tsx

--- app/form.tsx
+++ app/form.tsx
@@ -23,6 +23,106 @@ type EventState = {
   voted?: boolean;
 };
 
+export function EventCreateForm() {
+  let formRef = useRef<HTMLFormElement>(null);
+  let [state, mutate] = useOptimistic(
+      { pending: false },
+      function createReducer(state, newEvent: EventState) {
+        if (newEvent.newEvent) {
+          return {
+            pending: newEvent.pending,
+          };
+        } else {
+          return {
+            pending: newEvent.pending,
+          };
+        }
+      },
+  );
+
+    let eventStub = {
+    id: uuidv4(),
+    created_at: new Date().getTime(),
+    title: "",
+    contractAddress: "",
+    network: 0,
+    checkoutURL: "",
+    eventImageURL: "",
+    registeredImageURL: "",
+    registeredLocationImageURL: "",
+    registeredTicketImageURL: "",
+  };
+  let saveWithNewEvent = saveEvent.bind(null, eventStub);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let [isPending, startTransition] = useTransition();
+
+  return (
+      <>
+        <div className="mx-8 w-full">
+          <form
+              className="relative my-8"
+              ref={formRef}
+              onSubmit={(event) => {
+                event.preventDefault();
+                let formData = new FormData(event.currentTarget);
+
+                let newEvent = {
+                  ...eventStub,
+                  title: formData.get("title") as string,
+                }
+
+                formRef.current?.reset();
+                startTransition(async () => {
+                  mutate({
+                    newEvent,
+                    pending: true,
+                  });
+
+                  await saveEvent(newEvent, formData);
+                });
+              }}
+          >
+            <input
+                aria-label="Event Title"
+                className="pl-3 pr-28 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
+                maxLength={150}
+                placeholder="Title..."
+                required
+                type="text"
+                name="title"
+            />
+            <input
+                aria-label="Contract Address"
+                className="pl-3 pr-28 py-3 mt-1 text-lg block w-full border border-gray-200 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-300"
+                maxLength={150}
+                placeholder="0x..."
+                required
+                type="text"
+                name="contractAddress"
+            />
+              <div className={"pt-2 flex justify-end"}>
+                  <button
+                      className={clsx(
+                          "flex items-center p-1 justify-center px-4 h-10 text-lg border bg-blue-500 text-white rounded-md w-24 focus:outline-none focus:ring focus:ring-blue-300 hover:bg-blue-700 focus:bg-blue-700",
+                          state.pending && "bg-gray-700 cursor-not-allowed",
+                      )}
+                      type="submit"
+                      disabled={state.pending}
+                  >
+                      Create
+                  </button>
+              </div>
+          </form>
+        </div>
+          <div className="w-full">
+          </div>
+      </>
+  );
+}
+
+/* ========================================= */
+
 
 export function PollCreateForm() {
   let formRef = useRef<HTMLFormElement>(null);
@@ -264,6 +364,3 @@ export function PollVoteForm({poll, viewResults}: { poll: Poll, viewResults?: bo
         </div>
 );
 }
-
-/*  removed this at line 321               {state.showResults ? <PollResults poll={poll}/> : <PollOptions poll={poll} onChange={handleVote}/>} */
-
