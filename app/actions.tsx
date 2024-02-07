3e5dde0c22f6f2b5a0d4b3253f07cf7c9dd7428f
Christopher Carfi <ccarfi@gmail.com>
2024-02-07 13:41:02 -0800

    Update actions.tsx

--- app/actions.tsx
+++ app/actions.tsx
@@ -6,35 +6,25 @@ import {UnlockEvent} from "./types";
 import {redirect} from "next/navigation";
 
 export async function saveEvent(event: UnlockEvent, formData: FormData) {
+  const networkValue = formData.get("network");
+  let network: number = 0;
+  if (typeof networkValue === 'string') {
+    network = Number(networkValue);
+  }
+  
   let newEvent = {
     ...event,
     created_at: Date.now(),
     title: formData.get("title") as string,
     slug: formData.get("slug") as string,
     contractAddress: formData.get("contractAddress") as string,
-    networkValue: formData.get("network"),        // need to do some typing below
+    network,
     checkoutURL: "testcheckoutURL" as string,
     eventImageURL: " test eventImageURL" as string,
     registeredImageURL: "test registeredImageURL" as string,
     registeredLocationImageURL: "test registeredLocationImageURL" as string,
     registeredTicketImageURL: "test registeredTicketImageURL" as string,   
-  }
-  
-  // Retrieve the value from the form data
-//  const networkValue = formData.get("network");
-
-  // Initialize a variable for the network number
-  let network: number;
-
-  // Check if the value is a string and not null or a File object
-  if (typeof networkValue === 'string') {
-    // Convert the string to a number
-    network = Number(networkValue);
-    newEvent.network = network;
-  } else {
-    // Handle the case where the value is not a string (or throw an error)
-    newEvent.network = 0; // default value, or throw an error
-  }
+  };
   
   console.log("newEvent:");
   console.log(newEvent);            
