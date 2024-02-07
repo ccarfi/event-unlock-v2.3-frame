0954ceda4525e386949695ba1a94ff9c32dda23d
Christopher Carfi <ccarfi@gmail.com>
2024-02-07 06:42:47 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
         <div className="flex justify-center items-center bg-black rounded-full w-16 sm:w-24 h-16 sm:h-24 my-8">
-  	  <img src={`${process.env['HOST']}/public/unlock-icon-round.png`} alt="Unlock Logo" />
+  	  <img src={`${process.env['HOST']}/unlock-icon-round.png`} alt="Unlock Logo" />
 	</div>
         <h1 className="text-lg sm:text-2xl font-bold mb-2">
           Unlock Event Frames v2.3
