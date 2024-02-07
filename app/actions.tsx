e7ad5434ec2d61dec7bdce43cd3fdaf5cfee84ee
Christopher Carfi <ccarfi@gmail.com>
2024-02-07 09:09:10 -0800

    Update actions.tsx

--- app/actions.tsx
+++ app/actions.tsx
@@ -6,30 +6,6 @@ import { revalidatePath } from "next/cache";
 import {UnlockEvent} from "./types";
 import {redirect} from "next/navigation";
 
-/*
-export async function savePoll(poll: Poll, formData: FormData) {
-  let newPoll = {
-    ...poll,
-    created_at: Date.now(),
-    title: formData.get("title") as string,
-    option1: formData.get("option1") as string,
-    option2: formData.get("option2") as string,
-    option3: formData.get("option3") as string,
-    option4: formData.get("option4") as string,
-  }
-  await kv.hset(`poll:${poll.id}`, newPoll);       // changed this from 'poll' to 'newPoll'
-  await kv.expire(`poll:${poll.id}`, POLL_EXPIRY);
-  await kv.zadd("polls_by_date", {
-    score: Number(poll.created_at),
-    member: newPoll.id,
-  });
-  
-  revalidatePath("/events");
-  redirect(`/events/${poll.id}`);
-}
-*/
-
-// export async function saveEvent(event: UnlockEvent) {
 export async function saveEvent(event: UnlockEvent, formData: FormData) {
   let newEvent = {
     ...event,
@@ -42,10 +18,6 @@ export async function saveEvent(event: UnlockEvent, formData: FormData) {
     registeredImageURL: "test registeredImageURL" as string,
     registeredLocationImageURL: "test registeredLocationImageURL" as string,
     registeredTicketImageURL: "test registeredTicketImageURL" as string,
-//    option1: "Register" as string,
-//    option2: "Show location" as string,
-//    option3: "See ticket" as string,
-//   option4: "" as string,
     
 /*
     created_at: Date.now(),
@@ -80,16 +52,3 @@ export async function saveEvent(event: UnlockEvent, formData: FormData) {
   revalidatePath("/events");
   redirect(`/events/${event.id}`);
 }
-
-/*
-export async function votePoll(poll: Poll, optionIndex: number) {
-  await kv.hincrby(`poll:${poll.id}`, `votes${optionIndex}`, 1);
-
-  revalidatePath(`/events/${poll.id}`);
-  redirect(`/events/${poll.id}?results=true`);
-}
-
-export async function redirectToPolls() {
-  redirect("/events");
-}
-*/
