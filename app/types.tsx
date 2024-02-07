8cc376b3d3672917f1bf1f56920cd18a75f02202
Christopher Carfi <ccarfi@gmail.com>
2024-02-07 09:08:14 -0800

    Update types.tsx

--- app/types.tsx
+++ app/types.tsx
@@ -1,19 +1,5 @@
-/*
-export type Poll = {
-  id: string;
-  title: string;
-  option1: string;
-  option2: string;
-  option3: string;
-  option4: string;
-  votes1: number;
-  votes2: number;
-  votes3: number;
-  votes4: number;
-  created_at: number;
-};
-*/
 
+// TODO: see if this next line is needed
 export const POLL_EXPIRY = 60 * 60 * 24 * 180; // Expire polls after 3 months
 
 export type UnlockEvent = {
