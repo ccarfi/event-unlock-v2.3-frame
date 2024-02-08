091324bc4e6ba1193668ec9d4d22911a796c74de
Christopher Carfi <ccarfi@gmail.com>
2024-02-07 21:17:04 -0800

    Create ImageTicketRegistered.tsx

---
+++ pages/api/ImageTicketRegistered.tsx
@@ -0,0 +1,56 @@
+import { ImageResponse } from '@vercel/og';
+ 
+export const config = {
+  runtime: 'edge',
+};
+
+ export default async function handler() {
+   const url = `https://locksmith.unlock-protocol.com/v2/events/privy-meetup`;
+   let ticketImageURL = 'Ticket image not available'; // Declare ticketImageURL here with a default value 
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(JSON.stringify(data));
+    const ticketImageURL = data.data.image;
+  } 
+  catch (error) {
+    console.log(error);
+  }
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'white',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: `url(${process.env['HOST']}/bgp.png)`,
+        }}
+      >
+       {ticketImageURL}
+      </div>
+
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
