6167aeef14322f289385c7f9bbedd2200e04a541
Christopher Carfi <ccarfi@gmail.com>
2024-02-07 20:50:20 -0800

    Update imageLocationRegistered.tsx

--- pages/api/imageLocationRegistered.tsx
+++ pages/api/imageLocationRegistered.tsx
@@ -39,47 +39,6 @@ export const config = {
   return new ImageResponse(
     (
       <div
-  style={{
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '1200px',
-    height: '630px',
-    padding: '50px',
-    background: 'white',
-    color: 'black',
-    fontSize: '24px',
-  }}
->
-  <div style={{ marginRight: '20px', flexShrink: 0 }}> {/* Prevents the wrapper from shrinking */}
-    <img
-      src={`${process.env['HOST']}/map-pin.png`}
-      alt="Map Pin"
-      style={{
-        width: '100px',
-        height: 'auto',
-      }}
-    />
-  </div>
-  <div
-    style={{
-      flexGrow: 1,
-    }}
-  >
-    {eventAddress}
-  </div>
-</div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
-}
-
-/*
-     
-      <div
         style={{
           fontSize: 40,
           color: 'black',
@@ -90,10 +49,16 @@ export const config = {
           textAlign: 'center',
           justifyContent: 'center',
           alignItems: 'center',
-          display: 'flex',
+          backgroundImage: `url(${process.env['HOST']}/map-pin.png)`,
         }}
       >
-  	    <img src={`${process.env['HOST']}/map-pin.png`} alt="Map Pin" />
        {eventAddress}
       </div>
-      */
+
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
