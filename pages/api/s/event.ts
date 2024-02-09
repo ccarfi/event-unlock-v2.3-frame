1c056e24edfbf68c6007ce121f6ddad67078e1f5
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 15:58:47 -0800

    Update event.ts

--- pages/api/s/event.ts
+++ pages/api/s/event.ts
@@ -7,6 +7,47 @@ import { balanceOf } from "@/src/lib/unlock";
 import { ImageResponse } from '@vercel/og';
 
 
+////////////////////////////////////////////////
+/*
+ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'white',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: `url(${process.env['HOST']}/bgp.png)`,
+          display: 'flex',
+        }}
+      >
+          <img 
+            src={ticketImageURL} 
+            alt="Ticket Image"
+            style={{
+              maxHeight: '50%', // Limits the image height to 50% of its parent container
+              maxWidth: '100%', // Ensures the image does not exceed the width of the container
+              objectFit: 'contain', // Keeps the aspect ratio of the image
+            }} 
+          />
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+*/
+
+
+
+////////////////////////////////////////////////
+
 const HUB_URL = process.env['HUB_URL']
 const client = HUB_URL ? getSSLHubRpcClient(HUB_URL) : undefined;
 
@@ -159,47 +200,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             if (buttonId === 3) {
                  action = "ticket";
                  if (isMember) {
-      //              imageUrl = eventImageURL;
+                    imageUrl = eventImageURL;
                     button1Action = "post";
                     register = false;
-                    imageUrl = ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 40,
-          color: 'white',
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundImage: `url(${process.env['HOST']}/bgp.png)`,
-          display: 'flex',
-        }}
-      >
-          <img 
-            src={ticketImageURL} 
-            alt="Ticket Image"
-            style={{
-              maxHeight: '50%', // Limits the image height to 50% of its parent container
-              maxWidth: '100%', // Ensures the image does not exceed the width of the container
-              objectFit: 'contain', // Keeps the aspect ratio of the image
-            }} 
-          />
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
-
-
-
-
-                     
                  }   
                  else {
                     imageUrl = `${process.env['HOST_DEV']}/api/imageTicketNotRegistered`;
