8bc27a1346ac934abc12e4bb55c9d0de841ece56
Christopher Carfi <ccarfi@gmail.com>
2024-02-08 23:06:09 -0800

    Update page.tsx

--- app/events/s/[slug]/page.tsx
+++ app/events/s/[slug]/page.tsx
@@ -3,53 +3,6 @@ import {UnlockEvent} from "@/app/types";
 import Head from "next/head";
 import {Metadata, ResolvingMetadata} from "next";
 
-/*    
-    // >>>>> i may not need this since it'll be coming from the JSON returned from the API and not the database
-
-async function getEvent(slug: string): Promise<UnlockEvent> {
-
-    console.log("Entered getEvent slug");
-
-    
-    let nullEvent = {
-        id: "",
-        title: "No event found",
-        slug: "",
-        contractAddress: "",
-        network: 0,
-        checkoutURL: "",
-        eventImageURL: "",
-        registeredImageURL: "",
-        registeredLocationImageURL: "",
-        registeredTicketImageURL: "",
-        created_at: 0,
-    };
-
-    try {
-        let event: UnlockEvent | null = await kv.hgetall(`event:${id}`);
-
-        if (!event) {
-            return nullEvent;
-        }
-
-        return event;
-    } catch (error) {
-        console.error(error);
-        return nullEvent;
-    }
-  
-}
-  */
-    // <<<<< i may not need this since it'll be coming from the JSON returned from the API and not the database
-
-    
-/*
-type Props = {
-    params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-}
-*/
-
 type Props = {
     params: { slug: string }
     searchParams: { [key: string]: string | string[] | undefined }
@@ -60,18 +13,8 @@ export async function generateMetadata(
     parent: ResolvingMetadata
 ): Promise<Metadata> {
 
-    console.log("Entered generateMetadata");
-/*
-    // >>>>>> if coming from database, we'll need this
-    // read route params
-    const id = params.id;
-    const event = await getEvent(id);
-    // <<<< if coming frorm database, we'll need this
-*/
     
-    // >>>>> if coming from API, use this
     const slug = params.slug;
-//    const event = await getEvent(slug);
     const url = `https://locksmith.unlock-protocol.com/v2/events/${slug}`;
     let ogImageURL = 'og image not available'; // Declare ogImageURL here with a default value 
     let eventTitle = 'event title not available'; // Declare eventTitle here with a default value
@@ -99,17 +42,13 @@ export async function generateMetadata(
       catch (error) {
         console.log(error);
       }
-    // <<<<< if coming from API, use this
-
 
 
     const fcMetadata: Record<string, string> = {
         "fc:frame": "vNext",
         "fc:frame:post_url": `${process.env['HOST']}/api/event?slug=${slug}&register=true&firstvisit=true`,
-//        "fc:frame:image": `${process.env['HOST']}/frame-webinar-share-627.png`,
         "fc:frame:image": `${ogImageURL}`,
         "fc:frame:image:aspect_ratio": `1:1`,
-//        "fc:frame:button:1:action": `post_redirect`,
         "fc:frame:button:1:action": `link`,
         "fc:frame:button:1:target": `${regLink}`,
     };
@@ -132,17 +71,12 @@ export async function generateMetadata(
     }
 }
 
-//export default async function Page({params}: { params: {id: string}}) {
-//    const event = await getEvent(params.id);    
 export default async function Page({params}: { params: {slug: string}}) {
-//    const event = await getEvent(params.slug);    
-
-    
     return(
         <>
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
-                    Your event has been created. Copy-paste the URL in the address bar into Warpcast.
+                    Your event frame has been created. Copy-paste the URL in the address bar into Warpcast.
                 </main>
             </div>
         </>
