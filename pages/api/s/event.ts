a63ab5cc4a74eecf82d42fb59706cb1a22305351
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 13:29:18 -0800

    Update event.ts
    starting surgery to get details from the API

--- pages/api/s/event.ts
+++ pages/api/s/event.ts
@@ -94,7 +94,89 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     );
                 })
             );
+            
+            //////
+// export async function generateMetadata(
+//    { params, searchParams }: Props,
+//     parent: ResolvingMetadata
+// ): Promise<Metadata> {
 
+//    const slug = params.slug;
+    // we were given the slug when we came in — call the API and get the details
+    
+    const url = `https://locksmith.unlock-protocol.com/v2/events/${eventSlug}`;
+ 
+    let ogImageURL = 'og image not available'; // Declare ogImageURL here with a default value 
+    let eventTitle = 'event title not available'; // Declare eventTitle here with a default value
+    let regLink = 'registration link not available';
+
+    try {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json'
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(JSON.stringify(data));
+        ogImageURL = data.data.image;
+        eventTitle = data.name;
+        regLink = data.eventUrl;
+        console.log(regLink);
+      } 
+      catch (error) {
+        console.log(error);
+      }
+
+    /* 
+        The commented-out array line(s) below lets us easily add new buttons to the frame when
+        we are ready to add them, or switch between different frame configs.
+        The line near the commented-out line is the simple case where we have just one 'Register' 
+        button that redirects to the Event Landing Page. 
+        Farcater just added the 'action' and 'target' pieces
+        which make this much easier than doing the 'redirect' approach that
+        they required last week.
+    */
+/*
+    const fcMetadata: Record<string, string> = {
+        "fc:frame": "vNext",
+        "fc:frame:post_url": `${process.env['HOST_DEV']}/api/s/event?slug=${slug}&id=9bfcbbb4-a37b-4ac4-a345-e7bc5472f4d6&register=true&firstvisit=true`,
+        "fc:frame:image": `${ogImageURL}`,
+        "fc:frame:image:aspect_ratio": `1:1`,
+        "fc:frame:button:1:action": `link`,
+        "fc:frame:button:1:target": `${regLink}`,
+    };
+    ["Register", "See location", "Show my ticket", ""].filter(o => o !== "").map((option, index) => {
+        fcMetadata[`fc:frame:button:${index + 1}`] = option;
+//    ["Register", "", "", ""].filter(o => o !== "").map((option, index) => {
+//        fcMetadata[`fc:frame:button:${index + 1}`] = option;  
+    })
+
+    return {
+        title: eventTitle,
+        openGraph: {                            // these og tags are what get shared OUTSIDE of warpcast
+            title: eventTitle,
+            images: ogImageURL,                 
+        },
+        other: {
+            ...fcMetadata,
+        },
+        metadataBase: new URL(process.env['HOST'] || '')
+    }
+}
+
+*/
+
+
+
+
+            //////
+            
             // The main bit. If user is a member (ticketholder who is registered), set image to one thing. If not a member, set to another.
 
             // Check to see if the user had a valid ticket
