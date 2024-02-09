5fd5f54af6f59368f505200d04cccb00d5bb8f9d
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 13:57:03 -0800

    Update event.ts

--- pages/api/s/event.ts
+++ pages/api/s/event.ts
@@ -254,7 +254,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           <meta property="og:image" content="${imageUrl}">
           <meta name="fc:frame" content="vNext">
           <meta name="fc:frame:image" content="${imageUrl}">
-          <meta name="fc:frame:post_url" content="${process.env['HOST_DEV']}/api/s/event?time=928&slug=${eventSlug}&id=${event.id}&register=${register ? 'true' : 'false'}">
+          <meta name="fc:frame:post_url" content="${process.env['HOST_DEV']}/api/s/event?slug=${eventSlug}&register=${register ? 'true' : 'false'}">
           <meta name="fc:frame:button:1" content="${button1Text}">
           <meta name="fc:frame:button:1:action" content="${button1Action}">
           <meta name="fc:frame:button:2" content="${button2Text}">
