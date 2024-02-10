c7f41e10d284993767cc4500b4fedfcf4c7758ec
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 16:27:42 -0800

    Update event.tsx

--- pages/api/s/event.tsx
+++ pages/api/s/event.tsx
@@ -291,9 +291,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             if (buttonId === 3) {
                  action = "ticket";
                  if (isMember) {
-                    imageUrl = eventImageURL;
+ //                   imageUrl = eventImageURL;
                     button1Action = "post";
                     register = false;
+
+   imageUrl = new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        Inlined response.
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
                  }   
                  else {
                     imageUrl = `${process.env['HOST_DEV']}/api/imageTicketNotRegistered`;
