b750fd49169d58e6c2427b138e75076e7182f3d3
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 17:03:13 -0800

    Update imageHandler.tsx

--- pages/api/imageHandler.tsx
+++ pages/api/imageHandler.tsx
@@ -41,4 +41,29 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
         catch {
         }
-      return;
+    return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'white',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundImage: `url(${process.env['HOST']}/bgp.png)`,
+          display: 'flex',
+        }}
+      >
+          {testString}
+        </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
