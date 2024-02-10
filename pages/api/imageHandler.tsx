4a27176f51362627c2a0f5df735b794986a5024a
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 18:05:46 -0800

    Update imageHandler.tsx

--- pages/api/imageHandler.tsx
+++ pages/api/imageHandler.tsx
@@ -48,6 +48,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
+    console.log("Entering imageHandler return");
   return new ImageResponse(
     (
       <div
