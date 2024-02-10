88c5612714f0123fed1f18ea15ed679b698abfd6
Christopher Carfi <ccarfi@gmail.com>
2024-02-09 19:02:21 -0800

    Update imageHandler.tsx

--- pages/api/imageHandler.tsx
+++ pages/api/imageHandler.tsx
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import {kv} from "@vercel/kv";
 import {getSSLHubRpcClient, Message} from "@farcaster/hub-nodejs";
 import { getUserAddresses } from "@/src/lib/farcaster";
