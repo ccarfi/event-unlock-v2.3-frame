e029d770939b3c5db6c884f427644a926b23fcb9
Christopher Carfi <ccarfi@gmail.com>
2024-02-12 16:41:11 -0800

    Update imageHandler.tsx

--- pages/api/imageHandler.tsx
+++ pages/api/imageHandler.tsx
@@ -96,23 +96,6 @@ export default async function handler(req: NextRequest, res: NextResponse) {
             catch (error) {
                 console.log(error);
             }
-/*
-                    <div
-        style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
-          display: 'flex',
-        }}
-      >
-      <img src={eventImageURL} />
-*/
   
   return new ImageResponse(
     (
