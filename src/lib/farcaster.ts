7177f2356680313094734713df99259166b15d06
Christopher Carfi <ccarfi@gmail.com>
2024-02-16 16:41:25 -0800

    Update farcaster.ts

--- src/lib/farcaster.ts
+++ src/lib/farcaster.ts
@@ -1,11 +1,38 @@
-import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+//import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 
 //const endpoint = "https://nemes.farcaster.xyz:2281";
 const endpoint = "https://api.neynar.com:2281";
 
-
 const version = "v1";
-const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
+
+const base = "https://api.neynar.com/";
+const apiKey = process.env.NEYNAR_API_KEY;
+
+/*
+const hash = "0x8ffed4e8fa53c6e22b85f678c9a53067826e846a";
+const url1 = `${base}v2/farcaster/cast?identifier=${hash}&type=hash`;
+const response1 = await fetch(url1, {
+  headers: {
+    accept: "application/json",
+    api_key: apiKey,
+  },
+});
+const cast = await response1.json();
+console.log(cast); // logs information about the cast
+*/
+
+const fid = 3;
+const url2 = `${base}v1/farcaster/user?fid=${fid}`;
+const response2 = await fetch(url2, {
+  headers: {
+    accept: "application/json",
+    api_key: apiKey,
+  },
+});
+const user = await response2.json();
+console.log(user); // logs information about the user
+
+
 
 function hexToBytes(hex: string) {
   for (var bytes = [], c = 0; c < hex.length; c += 2) {
